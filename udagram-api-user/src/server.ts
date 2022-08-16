4ae51bb7bdb2241c22729d26cefd25744bc7c42f
tanbc0901 <tanbc0901@gmail.com>
2022-08-16 16:33:43 +0700

    final commit for p3

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -47,4 +47,4 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
     console.log( `server running ${config.url}` );
     console.log( `press CTRL+C to stop server` );
   } );
-})();
+})().catch((e) => { console.log(e)});;
